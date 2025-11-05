893cafbc76fefe0ef8b31dfdaff59abf3288dcef
sergio <sergio.gomez.30@lacetania.cat>
2025-11-05 19:17:04 +0100

    static_settings

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = 'users.CustomUser'
